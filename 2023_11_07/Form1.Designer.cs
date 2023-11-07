04845fcbc7167e0991d946b6d12757c03abe785e
박민성 <minsung060607@gmail.com>
2023-11-07 22:59:23 +0900

    Add files via upload

---
+++ 2023_11_07/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace _2023_11_07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(370, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(408, 399);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(117, 255);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(117, 282);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(117, 309);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 21);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(117, 336);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 21);
+            this.textBox8.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "on";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 39);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 2023_11_07/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Newtonsoft.Json.Linq;
+
+namespace _2023_11_07
+{
+    public partial class Form1 : Form
+    {
+        //전역으로 클래스선언
+        MqttClient client;
+        string clientId;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MQTT 브로커와 연결하는 부분
+            string BrokerAddress = "broker.mqtt-dashboard.com";
+            client = new MqttClient(BrokerAddress);
+
+            // 구독 신청을 해서 MQTT메시지를 어떻게 할래
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            // use a unique id as client id, each time we start the application
+            clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            //서버와 클라이언트가 접속이 완료되는 지점
+            //iot보드가 pulish하는 topic 을 subscr ibe해야 한다
+            //Subscribe Topic 추가
+            string[] mytopic =
+            {
+                /*
+                "bssm_iot/room1/sensor",
+                "bssm_iot/room2/sensor",
+                "bssm_iot/livingroom/dust",
+                "bssm_iot/livingroom/co2"
+                */
+                //"bssm_iot/livingroom/rfid"
+                //"bssm_iot/outdoor/weather"
+                "bssm_iot/"
+            };
+
+            byte[] myqos =
+            {
+                /*
+                0,
+                0,
+                0,
+                0
+                */
+                0
+            };
+            //구독 
+            client.Subscribe(mytopic,myqos);
+        }
+        //메시지 수신부
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            richTextBox1.Text +=$"[{e.Topic}]"+ ReceivedMessage + "\n";
+            //DO SOMETHING..!
+            //e.Topic
+            if(e.Topic == "bssm_iot/room1/sensor")
+            {
+                try
+                {
+                    //만약 json데이터가 불완전하다면 예외가 발생한다
+                    JObject myjson = JObject.Parse(ReceivedMessage);
+                    textBox1.Text = myjson["temp"].ToString();
+                    textBox2.Text = myjson["humi"].ToString();
+                    textBox3.Text = myjson["cds"].ToString();
+                    textBox4.Text = myjson["gas"].ToString();
+
+                }
+                catch
+                {
+                    try
+                    {
+                        //만약 json데이터가 불완전하다면 예외가 발생한다
+                        JObject myjson = JObject.Parse(ReceivedMessage);
+                        textBox1.Text = myjson["temp"].ToString();
+                        textBox2.Text = myjson["humi"].ToString();
+                        textBox3.Text = myjson["cds"].ToString();
+                        textBox4.Text = myjson["gas"].ToString();
+
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            else if(e.Topic == "bssm_iot/room2/sensor")
+            {
+                try
+                {
+                    //만약 json데이터가 불완전하다면 예외가 발생한다
+                    JObject myjson = JObject.Parse(ReceivedMessage);
+                    textBox5.Text = myjson["temp"].ToString();
+                    textBox6.Text = myjson["humi"].ToString();
+                    textBox7.Text = myjson["cds"].ToString();
+                    textBox8.Text = myjson["gas"].ToString();
+
+                }
+                catch
+                {
+
+                }
+            }
+            //json 데이터 parse
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //MQTT 연결 종료
+            client.Disconnect();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Publish("bssm_iot/living/humi",Encoding.UTF8.GetBytes("1"),0,false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            client.Publish("bssm_iot/living/humi", Encoding.UTF8.GetBytes("0"), 0, false);
+        }
+    }
+}
